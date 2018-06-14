255a17cb0245ae6cf44c8b2087e782debc8cedf4
Maximilian Falkenstein <falkensm@ethz.ch>
2018-06-14 17:29:17 +0200

    CI: Add smoke test

---
+++ .circleci/smoke-test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+export LANG=C
+set -e
+
+build/cinit | tee log.txt
+
+echo "Checking ping output"
+cat log.txt | grep '\[ping\]' >> /dev/null
+echo "ok"
+echo "Checking detailed ping output"
+cat log.txt | grep '\[ping\]' | grep "64 bytes" >> /dev/null
+cat log.txt | grep '\[ping\]' | grep "'CAP_NET_RAW' enabled" >> /dev/null
+echo "ok"
+
+echo "Checking failping output"
+cat log.txt | grep '\[failping\]' >> /dev/null
+echo "ok"
+echo "Checking detailed failping output"
+cat log.txt | grep '\[failping\]' | grep 'ping: socket: Operation not permitted' >> /dev/null
+echo "ok"
