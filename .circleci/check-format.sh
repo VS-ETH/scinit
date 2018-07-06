f364f6a33c1ed1cb3dc0338e2489ff5941f96390
Maximilian Falkenstein <maxf@njsm.de>
2018-07-06 11:44:58 +0200

    CI: Hopefully fix format check

--- .circleci/check-format.sh
+++ .circleci/check-format.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 echo "Checking code format"
-if [ -n $(git diff --name-only | grep -E "^(src|test).*(.cpp$|.h)$") ] ; then
+CHANGED_FILES="$(git diff --name-only | grep -E '^(src|test).*(.cpp$|.h)$')"
+if [ -n "${CHANGED_FILES}" ] ; then
     echo "#####################################################################"
     echo "### ERROR: Inconsistent formatting, please fix files and resubmit ###"
     echo "#####################################################################"
-    git diff --name-only | grep -E "^(src|test).*(.cpp$|.h)$" | xargs git diff
-    return 255
+    for i in ${CHANGED_FILES} ; do
+        git diff "${i}"
+    done
+    exit 255
 fi
